c0ba6d7b40ad563078ca8958566fc003180df9cf
Noelia Donato <noeliadonato@gmail.com>
2019-11-30 20:34:03 -0300

    clase 47, con ejercicios

---
+++ 11_Noviembre/Clase_47/README.md
@@ -0,0 +1,10 @@
+# Clase: 47
+* **Fecha:** 27/11/2019
+* **Objetivo de la clase:** Javascript.   
+* **Resumen de la clase:**
+> "Qué son las funciones. Para qué se utilizan. Código reutilizable. Asignando funciones a una variable. Estructura de una función. Funciones flecha. Sintaxis de funciones flecha. Invocando funciones. Scope de funciones. Variables dentro de una función. Parámetros. Nombres de parámetros. Parámetros por default.
+
+Links de la clase de hoy
+
+https://github.com/Ada-IT/bootcamp-frontend/blob/master/03_javascript/funciones/ejercicios/fn_001.md
+https://github.com/Ada-IT/bootcamp-frontend/blob/master/03_javascript/funciones/ejercicios/fn_002.md"
---
+++ 11_Noviembre/Clase_47/ejercicios.js
@@ -0,0 +1,64 @@
+
+/*FUNCIONES
+
+const unaFuncion = () => {
+	// cualquier codigo de js
+}
+
+const unaFuncion = (parametro = "") => {
+	// cualquier codigo de js
+	console.log("Valor del parametro: " + parametro);
+	const otraFuncion = () => {
+	}
+}
+
+unaFuncion("holis");
+unaFuncion("chauz");
+
+La funcion declarada adentro de la funcion, solo puede ser usada por ella.
+La funcion padre, puede ser usada desde afuera de ese codigo.
+parametro = "", significa que primero va a tomar el valor que le mandan, y sino por defecto lo va a poner vacio, así no se le muestra al usuario -> undefined
+
+-----------------------------------------------------------------------------
+*/
+
+/*
+console.log(`Ejercicio Funciones 001
+Declarar una función que muestre en consola los números pares del 0 al 100`);
+
+const numPares = () => {
+
+	for (let i = 0; i <= 100; i++) {
+		if (i % 2 === 0) {
+			console.log(`${i} es un numero par`);
+		}
+	}
+
+}
+
+numPares();
+
+console.log("-----------------------------------------------------------------");
+
+console.log(`Ejercicio Funciones 002
+Declarar una función que muestre los siguientes datos en consola:
+Tu Nombre
+Edad
+Teléfono
+Calle
+Altura
+Código postal`);
+
+const misDatos = () => {
+	console.log(`
+	Tu Nombre: Noelia
+	Edad: 33
+	Teléfono: 0303456
+	Calle: Av Siempre Viva
+	Altura: 742
+	Código postal: 1007`);
+}
+
+misDatos();
+console.log("-----------------------------------------------------------------");
+*/
