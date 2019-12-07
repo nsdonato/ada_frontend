2381ced00431917daa2cf473e8640f0d01122ed5
Noelia Donato <noeliadonato@gmail.com>
2019-12-07 15:18:50 -0300

    ejercicio 51, clase 51

--- 12_Diciembre/Clase_51/README.md
+++ 12_Diciembre/Clase_51/README.md
@@ -2,4 +2,8 @@
 * **Fecha:** 06/12/2019
 * **Objetivo de la clase:** Javascript.   
 * **Resumen de la clase:**
-> Return implicito en funciones flecha. Sintaxis de funciones flecha: cuando son opcionales parentesis y llaves. Funciones tradicionales, diferencias. 
+> Como javascript llama distintas funciones. Que es el callstack. Como inspeccionarlo en el navegador. Recibiendo funciones como parametros: callbacks. 
+
+Links de la clase de hoy
+
+https://github.com/malerey/Tarea-clase-51
--- 12_Diciembre/Clase_51/ejercicios.js
+++ 12_Diciembre/Clase_51/ejercicios.js
@@ -11,13 +11,57 @@ Callbacks:
 
 
 /*-----------------------------  EJERCICIOS -------------------------------------------------------------
-*/
-// falta que male pase el ejercicio..
-let password = 12;
 
-const cumpleCaracteres = () => {
-	return password.length > 6;
+Ejercicio que dejó Male para hacer:
+Hacer un codigo que determine si un password es valido o no.
+
+Hacer una funcion que reciba un string como parametro. La funcion debe retornar true si el parametro:
+
+Tiene seis caracteres o mas
+Tiene al menos un numero
+Tiene al menos una letra
+Y false en caso contrario.
+
+El archivo 51.js tiene ya fragmentos de codigo que pueden servirles para hacer las funciones
+*/
+
+let password = prompt("Ingrese su password"); //"lili12"
+
+const numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+debugger;
+// la funcion que chequea el largo es mas o menos asi:
+const LargoValido = (password) => password.length >= 6;
+
+// la funcion que chequea si tiene numeros es mas o menos asi:
+const TieneNumeros = () => {
+	for (let i = 0; i < password.length; i++) {
+		if (numeros.indexOf(password[i]) != -1) {
+			return true;
+		}
+	}
+	return false;
 }
-const esPasswordValido = () => {
-	//if (cumpleCaracteres && 
+
+const TieneLetras = () => {
+	// la funcion que chequea si tiene letras  es mas o menos asi:
+	for (let i = 0; i < password.length; i++) {
+		if (letras.indexOf(password[i]) != -1) {
+			return true;
+		}
+	}
+	return false;
 }
+
+
+const VerificarPassword = (password) => {
+	if (LargoValido(password) && TieneNumeros(password) && TieneLetras(password)) {
+		alert("Password valido");
+	} else {
+		alert("Por favor verificar el password");
+	}
+}
+
+VerificarPassword(password);
+
