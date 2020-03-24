15142b70a455b4d7a51f9f1cbd8b874fdfb873ab
Noelia Donato <noeliadonato@gmail.com>
2020-03-24 14:19:53 -0300

    Clase 82, Clase 88

---
+++ 02_Febrero_2020/Clase_82/main.js
@@ -0,0 +1,20 @@
+/*
+*Comandos que usamos en la clase de hoy*
+
+    Instalar create-react-app: en la consola, en cualquier carpeta, escribimos: npm i -g create-react-app
+    Para hacer un proyecto nuevo: con la consola vamos a una carpeta, por ejemplo, al escritorio, y escribimos: npx create-react-app nombre-proyecto (nombre-proyecto se puede cambiar por lo que queramos).
+    Vamos a ver que se crea una carpeta nueva. Vamos a esa carpeta, y escribimos: npm start
+    Si todo sale bien, se debe abrir una ventana nueva en el navegador donde vamos a ver nuestro proyecto de React.
+    Arrastramos la carpeta entera hasta Visual Studio, y vamos a ver que se abre todo el proyecto. Desde Visual Studio podemos abrir la carpeta src y ahi esta App.js que es donde trabajamos la clase de hoy.
+
+    React es una libreria de Javascript, simplifica la creación de una página web.
+    Cuando hago cambios en el DOM:
+        Virtual Dom: copia del dom, si cambio algo, react verifica la diferencia y solo cambia eso.
+        Dom: cambia toda la página
+
+    Los componentes son funciones de JavaScript.
+    Retornan JSX -> JavaScript con Html
+    ./ misma carpeta
+    ../ una carpeta mas arriba
+
+*/
